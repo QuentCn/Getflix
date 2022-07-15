d8c6bc7bded7025cbbc1526128ee9964c33f7eaa
Quentin Coene <quentin.coene.pro@gmail.com>
2022-07-15 14:39:04 +0200

    front-end

---
+++ asset/js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+    ('use strict');
+
+    $(".menu-toggle").on("click", function () {
+        else.preventDefault();
+        $("sidebar-wrapper").toggleClass("active");
+        $("")
+    })
+})
+    (jQuery);
