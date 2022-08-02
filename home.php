9f4c35dc2b7001640092c3d80a0a0ffc11be0229
Clement-Leger <clementleger4@gmail.com>
2022-08-02 15:22:54 +0200

    intégration de la section commentaire

--- home.php
+++ home.php
@@ -182,8 +182,107 @@ js pas encore fait -->
 
 
 
+<!-- ----------------------------------- TEST ZONE COMMENTAIRE ------------------------------------- -->
+
+<?php
+if($_SESSION['fullname']){
+ echo $_SESSION['fullname'] . ' son email est ' . $_SESSION['email'] . ' son id est ' . $_SESSION['user_id'] . ' est bien connecté <br />';
+};
+
+$dataform = $db->prepare('SELECT * FROM `users`');
+$dataCom = $db->prepare('SELECT * FROM `comments`');
+
+$dataCom->execute();
+$dataFetchCom = $dataCom->fetchAll();
+
+$dataform->execute();
+$dataFetch = $dataform->fetchAll();
+
+$dataComId = $db->prepare('SELECT `commentID` FROM `comments`');
+$dataComId->execute();
+$commentID = $dataComId->fetch()['commentID'];
 
 
+
+// -------------------------------- INSERTION DU COMMENTAIRE ---------------------------
+
+//CONDITION
+
+    //Si les champs ne sont pas remplis il ne se passe rien !
+    if(isset($_POST['submit'])){
+      if(empty($_POST["comment"])){
+      echo"Veuillez remplir tous les champs !";
+      }};
+      
+  if(isset($_POST['submit'])){
+       if(isset($_POST['submit'])){
+          if(!empty($_POST["comment"])) {
+              $user_id = $_SESSION["user_id"];
+              $fullname = htmlspecialchars($_SESSION["fullname"]); //htmlspecialchars permet d'empêcher un utilisateur malveillant d'envoyer du code via l'input
+              $email = htmlspecialchars($_SESSION["email"]);
+              $comment = $_POST['comment'];
+              
+               //On va selectionner le tout ("*") dans le table users
+              $dataform = $db->prepare('SELECT * FROM `users`');
+              $dataCom = $db->prepare('SELECT * FROM `comments`');
+          
+               //On va tout récupérer
+              
+              $dataCom->execute();
+              $dataFetchCom = $dataCom->fetchAll();
+
+              $dataform->execute();
+              $dataFetch = $dataform->fetchAll();
+              $id = $_POST["user_id"];
+          
+              $sqlQuery = 'INSERT INTO `comments`(user_id, fullname, email, comment) VALUES (:user_id, :fullname, :email, :comment)';
+              
+              // Préparation
+              $insertData = $db->prepare($sqlQuery);
+
+              // Exécution, le message est maintenant dans la base de données
+              $insertData->execute([
+                  'user_id' => $user_id,
+                  'fullname' => $fullname,
+                  'email' => $email,
+                  'comment' => $comment,
+              ]);
+    }}};
+                  
+?>
+    <form action="" method="POST">
+    <input type="text" name="comment" style="width: 450px; height: 200px">
+    <button type="submit" name="submit">Envoyer votre commentaire</button>
+    </form>
+
+            <table id="commentSection">
+            <tr>
+                    <th>Id</th>
+                    <th>fullname</th>
+                    <th>Email</th>
+                    <th>Comment</th>
+                    <?php if($_SESSION['administrateur']){echo "<th>Editer</th>
+                    <th>Supprimer</th>";
+                    };?>
+            </tr> 
+            <?php //on va afficher ce qu'on veut qui provient de table users
+            foreach ($dataFetchCom as $dataFetchCom) {
+            ?>
+            <tr> 
+            <td><?php echo $dataFetchCom['user_id']; ?></td>
+            <td><?php echo $dataFetchCom['fullname']; ?></td>
+            <td><?php echo $dataFetchCom['email']; ?></td>
+            <td><?php echo $dataFetchCom['comment']; ?></td>
+            <?php if($_SESSION['administrateur']){echo
+            '<td><a href="updateCom.php?commentID=';?><?php echo $dataFetchCom["commentID"];?><?php echo'" name="edit"><img class="icons" src="../asset/img/wrench.png" alt="clé à molette"></img></a></td>
+            <td><a href="deleteCom.php?commentID=';?><?php echo $dataFetchCom["commentID"];?><?php echo'" name="delete"><img class="icons" src="../asset/img/cross.png" alt="croix"></img></a></td>';
+        };?>
+
+            </tr>
+
+            <?php
+            }?>
+            </table>
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
